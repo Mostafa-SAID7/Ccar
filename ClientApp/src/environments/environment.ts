de95ddcf443d5502551f625845e813dc123c8d4d
Mostafa-SAID7 <mostafa-said7@github.com>
2025-11-25 22:14:47 +0200

    feat: Add initial client application configuration and project build outputs.

--- ClientApp/src/environments/environment.ts
+++ ClientApp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000/api',
+    apiUrl: 'http://localhost:5031/api',
     appName: 'Ccar Community',
     version: '1.0.0',
     features: {
